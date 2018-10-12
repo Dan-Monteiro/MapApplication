f30eacc5c20f85d8af461cd49b6682a0456ac82d
Daniel Monteiro <daniel.monteirofilho@outlook.com>
2018-10-12 15:48:19 -0300

    Implementação Escola tipod e mapa e mudança de marcadores

--- app/src/main/java/br/com/daniel/mapaplication/MainActivity.java
+++ app/src/main/java/br/com/daniel/mapaplication/MainActivity.java
@@ -3,15 +3,48 @@ package br.com.daniel.mapaplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonMapNormal = findViewById(R.id.mapaNormal);
+        buttonMapNormal.setOnClickListener(this);
+        Button buttonMapHibrido = findViewById(R.id.mapaHibrido);
+        buttonMapHibrido.setOnClickListener(this);
+        Button buttonMapTerreno = findViewById(R.id.mapaTerreno);
+        buttonMapTerreno.setOnClickListener(this);
+        Button buttonMapSatelite = findViewById(R.id.mapaSatelite);
+        buttonMapSatelite.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        int mapType = 0;
+        
+        if(id == R.id.mapaNormal){
+            mapType = GoogleMap.MAP_TYPE_NORMAL;
+        }else if(id == R.id.mapaHibrido){
+            mapType = GoogleMap.MAP_TYPE_HYBRID;
+        }else if(id == R.id.mapaTerreno){
+            mapType = GoogleMap.MAP_TYPE_TERRAIN;
+        }else if(id == R.id.mapaSatelite){
+            mapType = GoogleMap.MAP_TYPE_SATELLITE;
+        }
+
         Intent intent = new Intent(this,MapsActivity.class);
+        intent.putExtra("TYPE", mapType);
         startActivity(intent);
     }
 }
--- app/src/main/java/br/com/daniel/mapaplication/MapsActivity.java
+++ app/src/main/java/br/com/daniel/mapaplication/MapsActivity.java
@@ -1,5 +1,6 @@
 package br.com.daniel.mapaplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
@@ -9,11 +10,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapClickListener {
 
     private SupportMapFragment  mapFragment;
+    private int mapType;
 
     @Override
     public void onMapClick(LatLng latLng) {
@@ -23,19 +28,37 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapCli
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.map_layout);
+        Intent i = getIntent();
+        mapType = i.getIntExtra("TYPE",0);
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                iniciarMapa(googleMap);
+                iniciarMapa(googleMap, mapType);
             }
         });
     }
 
-    private void iniciarMapa(GoogleMap map) {
-        LatLng fafica = new LatLng(-8.295195, -35.979096);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(fafica, 20);
+    private void iniciarMapa(GoogleMap map, int mapType) {
+
+        map.setMapType(mapType);
+
+        int icon = 0;
+
+            if(mapType % 2 == 0){
+                icon = R.drawable.dart_board;
+            }else{
+                icon = R.drawable.objective;
+            }
+
+        LatLng localizacao = new LatLng(-8.295195, -35.979096);
+        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(icon);
+        map.addMarker(new MarkerOptions().position(localizacao)
+                .title("Minha Rua"))
+                .setIcon(bitmapDescriptor);
+
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(localizacao, 20);
         map.animateCamera(update);
         }
     }
